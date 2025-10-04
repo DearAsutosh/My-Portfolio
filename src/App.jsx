24582843da812837221cd84bdb68d7d63b555613
Asutosh Sahoo <kanhaasu2005@gmail.com>
2025-10-04 10:34:08 +0530

    fix: scroll-percentage bar fixed

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,33 @@ import Contact from './components/Pages/ContactSection/Contact'
 import Footer from './components/Pages/Footer/Footer'
 import Loader from './components/NeonLoader'
 import SmoothCursor from './components/SmoothCursor'
-import { motion } from 'motion/react'
+import { motion, useScroll } from 'motion/react'
 
 const App = () => {
   const [loading, setLoading] = React.useState(true)
+  const scrollYProgress = useScroll().scrollYProgress
 
   return (
     <>
       {/* Loader */}
       {loading && <Loader onFinish={() => setLoading(false)} />}
 
+      {/* Scroll progress bar (fade in after loader finishes) */}
+      {!loading && (
+        <motion.div
+          className="fixed top-0 h-1 w-full bg-cyan-400 origin-top-left z-50 border border-black"
+          style={{ scaleX: scrollYProgress }}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.7, ease: 'easeInOut' }}
+        />
+      )}
+
       {/* App content */}
       <motion.div
         className={`transition-opacity duration-700 ${loading ? 'opacity-0' : 'opacity-100'}`}
       >
         <SmoothCursor />
-        <motion.div className="fixed top-0 h-1 w-full bg-cyan-400 origin-top-left z-50 border border-black"></motion.div>
         <LandingPage />
         <SkillsSection />
         <ProjectsSection />
